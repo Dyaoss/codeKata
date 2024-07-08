b56dc2c0a897b8a74df7c2498c94b29b30e1d54c
Dyaoss <94600691+Dyaoss@users.noreply.github.com>
2024-07-08 09:40:30 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.03 ms, Memory: 62.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.kt
@@ -0,0 +1,14 @@
+class Solution {
+    fun solution(s: String): Boolean {
+        var answer = true
+
+        if (s.length in 1..8) {
+            if (s.length == 4 || s.length == 6) {
+                if (s.all { it.isDigit() }) {
+                    answer = true
+                } else answer = false
+            }else answer = false
+        }else answer = false
+        return answer
+    }
+}
