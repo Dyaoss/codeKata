3e3ec83a03129c60416a7943ccc578802ea417c3
Dyaoss <94600691+Dyaoss@users.noreply.github.com>
2024-07-14 15:34:55 +0900

    [level 1] Title: 직사각형 별찍기, Time: 225.59 ms, Memory: 63.6 MB -BaekjoonHub

--- 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.kt
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.kt
@@ -1,8 +1,8 @@
 fun main(args: Array<String>) {
     val (a, b) = readLine()!!.split(' ').map(String::toInt)
-    
-    repeat(b.toInt()){
-        repeat(a.toInt()){
+
+    for (i in 1..b.toInt()) {
+        for (i in 1..a.toInt()) {
             print('*')
         }
         println("")
