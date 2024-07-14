d85584ef3dc0db7f416be4de7a5a4b853aa8efdb
Dyaoss <94600691+Dyaoss@users.noreply.github.com>
2024-07-14 16:45:08 +0900

    [level 1] Title: 3진법 뒤집기, Time: 9.03 ms, Memory: 62.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.kt
@@ -0,0 +1,39 @@
+import kotlin.math.pow
+import kotlin.math.sqrt
+
+class Solution {
+    fun solution(n: Int): Int {
+        var answer: Int = 0
+        var reversed = MakeBase3(n)
+        var toBase10 = ReverseArray(reversed)
+        answer = ConvertToBase10(toBase10)
+
+        return answer
+    }
+}
+
+fun MakeBase3(x: Int): MutableList<Int> {
+    var a = x
+    var base3 = mutableListOf<Int>()
+
+    while (a / 3 > 0) {
+        base3.add(a % 3)
+        a = (a / 3)
+    }
+    base3.add(a % 3)
+
+    return base3
+}
+
+fun ReverseArray(x: MutableList<Int>): MutableList<Int> {
+    return x.reversed().toMutableList()
+}
+
+fun ConvertToBase10(x: MutableList<Int>): Int {
+    var total = 0
+
+    for (i in 0..x.size - 1) {
+        total += x[i] * 3.0.pow(i).toInt()
+    }
+    return total
+}
